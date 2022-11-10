32006845f8bab025dda250bf1f8503a86b889733
Imam Hossain <imamhossainpro@gmail.com>
2022-11-11 02:32:39 +0600

    Hero Section Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
